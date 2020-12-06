8f8f383dede87746c9fe501be35d04a3260de770
maylacdao <may.lacdao@gmail.com>
2020-12-05 22:32:38 -0500

    More edits to app and notebook

--- Homework/Instructions/app.py
+++ Homework/Instructions/app.py
@@ -1,131 +1,112 @@
-from flask import Flask, render_template, redirect, jsonify
+# python modules
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-
-#dependencies
+# sql modules
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy import func
+from sqlalchemy import create_engine, func
 
-import pandas as pd
-import numpy as np
-import datetime
+# flask modules
+from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-engine = create_engine("sqlite:///hawaii.sqlite")
+# set up SQLAlchemy
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-Measurements = Base.classes.measurement
-Stations = Base.classes.station
+# print(Base.classes.keys())
 
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# session
 session = Session(engine)
 
-@app.route("/")
-def home():
-	print("Server received request for 'Home' page.")
-	return "Welcome to the Surfs Up Weather API!"
+# Flask app
+app = Flask(__name__)
 
-@app.route("/welcome")
-#List all available routes
-def welcome ():
-	return (
-		f"Welcome to the Surf Up API<br>"
-		f"Available Routes:<br>"
-		f"/api/v1.0/precipitation<br>"
-		f"/api/v1.0/stations<br>"
-		f"/api/v1.0/tobs<br>"
-		f"/api/v1.0/<start><br>"
-		f"/api/v1.0<start>/<end><br>"
-	)
-	
+
+@app.route("/")
+def main():
+    return "<html>
+                <style>
+                    body {
+                        background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);        
+                    }
+
+                    h1{
+                        font-size: 60;
+                        text-align: center;
+                        padding-top: 100px;
+                    }
+                </style>
+                <body>
+                    <h1>Welcome to the</h1>
+                </body>
+            </html>" 
+
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-	#Query for the dates and temperature observations from the last year.
-	results = session.query(Measurements.date,Measurements.prcp).filter(Measurements.date >= "08-23-2017").all()
+    prcp_data = session.query(Measurement.date, Measurement.prcp).\
+        order_by(Measurement.date.desc()).all()
+    session.close()
+    prcp_data_list = dict(prcp_data)
+    return jsonify(prcp_data_list)
 
-	year_prcp = list(np.ravel(results))
-	#results.___dict___
-	#Create a dictionary using 'date' as the key and 'prcp' as the value.
-	"""year_prcp = []
-	for result in results:
-		row = {}
-		row[Measurements.date] = row[Measurements.prcp]
-		year_prcp.append(row)"""
-
-	return jsonify(year_prcp)
 
 @app.route("/api/v1.0/stations")
 def stations():
-	#return a json list of stations from the dataset.
-	results = session.query(Stations.station).all()
+    all_stations = session.query(Station.station, Station.name).all()
+    session.close()
+    stations_list = list(all_stations)
+    return jsonify(stations_list)
 
-	all_stations = list(np.ravel(results))
-
-	return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
-def temperature():
-	#Return a json list of Temperature Observations (tobs) for the previous year
-	year_tobs = []
-	results = session.query(Measurements.tobs).filter(Measurements.date >= "08-23-2017").all()
+def tobs():
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    tobs_query = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.date >= year_ago).all()
+    session.close()
+    tobs_list = list(np.ravel(tobs_query))
+    return jsonify(tobs_list)
 
-	year_tobs = list(np.ravel(results))
-
-	return jsonify(year_tobs)
 
 @app.route("/api/v1.0/<start>")
-def start_trip_temp(start_date):
-	start_trip = []
+def oneDate(start):
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
 
-	results_min = session.query(func.min(Measurements.tobs)).filter(Measurements.date == start_date).all()
-	results_max = session.query(func.max(Measurements.tobs)).filter(Measurements.date == start_date).all()
-	results_avg = session.query(func.avg(Measurements.tobs)).filter(Measurements.date == start_date).all()
+    temp_info = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    session.close()
+    temp_data = list(np.ravel(temp_info))
+    return jsonify(temp_data)
 
-	start_trip = list(np.ravel(results_min,results_max, results_avg))
-
-	return jsonify(start_trip)
-
-def greater_start_date(start_date):
-
-	start_trip_date_temps = []
-
-	results_min = session.query(func.min(Measurements.tobs)).filter(Measurements.date >= start_date).all()
-	results_max = session.query(func.max(Measurements.tobs)).filter(Measurements.date >= start_date).all()
-	results_avg = session.query(func.avg(Measurements.tobs)).filter(Measurements.date >= start_date).all()
-
-	start_trip_date_temps = list(np.ravel(results_min,results_max, results_avg))
-
-	return jsonify(start_trip_date_temps)
 
 @app.route("/api/v1.0/<start>/<end>")
+def calc_temps(start, end):
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
+    end_date = dt.datetime.strptime(end, "%Y-%m-%d")
 
-def start_end_trip(start_date, end_date):
+    if end != "":
+        temp_info = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start_date).filter(
+                Measurement.date <= end_date).all()
+        session.close()
+        temp_data = list(np.ravel(temp_info))
+        return jsonify(temp_data)
 
-	start_end_trip_temps = []
-
-	results_min = session.query(func.min(Measurements.tobs)).filter(Measurements.date == start_date, Measurements.date == end_date).all()
-	results_max = session.query(func.max(Measurements.tobs)).filter(Measurements.date == start_date, Measurements.date == end_date).all()
-	results_avg = session.query(func.avg(Measurements.tobs)).filter(Measurements.date == start_date, Measurements.date == end_date).all()
-
-	start_end_trip_temps = list(np.ravel(results_min,results_max, results_avg))
-
-	return jsonify(start_end_trip_temps)
-
-def start_end_trip(start_date, end_date):
-
-	round_trip_temps = []
-
-	results_min = session.query(func.min(Measurements.tobs)).filter(Measurements.date >= start_date, Measurements.date >= end_date).all()
-	results_max = session.query(func.max(Measurements.tobs)).filter(Measurements.date >= start_date, Measurements.date >= end_date).all()
-	results_avg = session.query(func.avg(Measurements.tobs)).filter(Measurements.date >= start_date, Measurements.date >= end_date).all()
-
-	round_trip_temps = list(np.ravel(results_min,results_max, results_avg))
-
-	return jsonify(round_trip_temps)
+    else:
+        temp_info = session.query(func.min(Measurement.tobs), func.avg(
+            Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date > end_date).all()
+        session.close()
+        temp_data = list(np.ravel(temp_info))
+        return jsonify(temp_data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ Homework/Instructions/climateApp.py
@@ -0,0 +1,3 @@
+#import dependencies
+#import pandas as pd
+#import numpy as np
